ced618dad9d4b6865bcaac950cf8a79af53dcc6c
Andrieli Nickhorn <andrieliclara2808@gmail.com>
2026-06-15 21:39:48 -0300

    docs: implemeta pesquisa

---
+++ pratica-01.js
@@ -0,0 +1,31 @@
+let nomeAlexa = "Alexa Rembowski";
+let idadeAlexa = "18";
+let cidadeAlexa = "Campinas";
+
+console.log("Nome: " + nomeAlexa);
+console.log("Idade: " + idadeAlexa + " anos");
+console.log("Cidade: " + cidadeAlexa);
+
+
+console.log("  ")
+
+
+let nomeAndrieli = "Andrieli Nickhorn";
+let idadeAndrieli = "18";
+let cidadeAndrieli = "Canoas";
+
+console.log("Nome: " + nomeAndrieli);
+console.log("Idade: " + idadeAndrieli + " anos");
+console.log("Cidade: " + cidadeAndrieli);
+
+
+console.log("  ")
+
+
+let nomeNicoly = "Nicoly Gomes";
+let idadeNicoly = "17";
+let cidadeNicoly = "Porto Alegre";
+
+console.log("Nome: " + nomeNicoly);
+console.log("Idade: " + idadeNicoly + " anos");
+console.log("Cidade: " + cidadeNicoly);
